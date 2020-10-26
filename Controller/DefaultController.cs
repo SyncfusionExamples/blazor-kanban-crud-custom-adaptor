1a557c8cc206150893f7dfc5415e9b145e28d12a
Rajendran R <rajendranr-5483@users.noreply.github.com>
2020-10-26 11:41:27 +0530

    The files added to perform CRUD operations.

---
+++ Controller/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using BlazorApp1.Data;
+using Newtonsoft.Json;
+using Syncfusion.Blazor;
+using Syncfusion.Blazor.Data;
+using System.Collections;
+
+namespace BlazorApp1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        OrderDataAccessLayer db = new OrderDataAccessLayer();
+        // GET: api/Default
+        [HttpGet]
+        public IQueryable<Order> Get()
+        {
+            IQueryable<Order> data = db.GetAllOrders();   //call the method to fecth data from db and return to client
+            return data;
+        }
+    }
+}
---
+++ Data/OrderDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class Order
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        public string ShipName { get; set; }
+        public string ShipCity { get; set; }
+
+    }
+}
